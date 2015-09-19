2614dc7a0d1420808bc53f90ee83bed0eb488dda
Ak <akshay.deep0@gmail.com>
2015-09-19 22:00:16 +0530

    use struct instead global

--- main.c
+++ main.c
@@ -1,105 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h> 
+#include <stdlib.h> 
 #include <string.h>
-
 #include <direct.h> // for _mkdir
-
 #include "./helper/code_gen.h" // load helper function
 
-#define WEB_ADDR_BUFF (100)
-#define XAMMP_DRIVE_BUFF (100)
-#define PATH_BUFF (300)
 
+typedef struct{
+    char *addr;
+    char *drive;
+    char *addr_location;
+    char *vhost;
+} Cli_info;
 
-// TODO
-// add more validations
-// use typedef :S
-// test buffer size
+static const char *host_path = "C:/Windows/System32/drivers/etc/hosts";
 
-static char webiste[WEB_ADDR_BUFF];        // website name
-static char xammp_drive[XAMMP_DRIVE_BUFF]; // dir where xammp is located 
-static char webiste_path[PATH_BUFF];
-static char xammp_path[PATH_BUFF];
-
-// for code update
-static char vhost_path[PATH_BUFF];
-static const char host_path[] = "C:/Windows/System32/drivers/etc/hosts";
-
-
-// restart appache
-// apache\bin\httpd.exe -k restart
-
-// create full path
-void create_site_dir()
+char* read_input(const char *msg, int len)
 {
+    char* buffer = (char *)malloc(sizeof(char) * (len+1));
 
-    if (_mkdir(webiste_path) == -1){
-        printf("Error: unable to create Dir \"%s\"\n", webiste_path);
-        exit(1);
+    fputs(msg, stdout);
+    fgets(buffer, len, stdin);
+
+    // remove New line
+    int last_char = strlen(buffer)-1;
+    if (buffer[last_char] == '\n'){
+        buffer[last_char] = '\0';
     }
+
+    return buffer;
 }
 
-
-void init_cli()
-{
-    fputs("Site Name: ", stdout);
-    fgets(webiste, WEB_ADDR_BUFF, stdin);
-
-    fputs("xammp Drive:", stdout);
-    fgets(xammp_drive, XAMMP_DRIVE_BUFF, stdin);
-
-    // remove new line char
-    webiste[strcspn(webiste, "\r\n")] = 0;
-    xammp_drive[strcspn(xammp_drive, "\r\n")] = 0;
-
-    // update var
-    strcpy(webiste_path, xammp_drive);
-    strcat(webiste_path, ":/xampp/htdocs/");
-    strcat(webiste_path, webiste);
-
-    strcpy(xammp_path, xammp_drive);
-    strcat(xammp_path, ":/xampp/");
-    
-    strcpy(vhost_path, xammp_path);
-    strcat(vhost_path, "apache/conf/extra/httpd-vhosts.conf");
-}
-
-// typedef struct
-// {
-//     char webiste[WEB_ADDR_BUFF];        // website name
-//     char websit_dir[PATH_BUFF];
-//     char xammp[XAMMP_DRIVE_BUFF]; // dir where xammp is located 
-//     char xammp_drive[PATH_BUFF];
-//     char vhost[PATH_BUFF];
-// } cli_info;
-
-
-// void init_cli_str()
-// {
-//     cli_info ci;   
-//     fputs("Site Name: ", stdout);
-//     fgets(ci.webiste, sizeof(ci.webiste), stdin);
-
-//     fputs("xammp Drive:", stdout);
-//     fgets(ci.xammp_drive, sizeof(ci.xammp_drive), stdin);
-
-//     // remove new line char
-//     ci.webiste[strcspn(ci.webiste, "\r\n")] = 0;
-//     ci.xammp_drive[strcspn(ci.xammp_drive, "\r\n")] = 0;
-
-//     // xammmp_drive:/xammp/htdocs/website
-//     snprintf(ci.websit_dir, sizeof(ci.websit_dir), 
-//             "%s%s%s", ci.xammp_drive,":/xampp/htdocs/", ci.webiste);
-
-//     // xammmp_drive:/xammp/
-//     snprintf(ci.xammp, sizeof(ci.xammp),
-//             "%s%s", ci.xammp_drive, ":/xampp/");
-    
-//     snprintf(ci.vhost, sizeof(ci.xammp),
-//             "%s%s", ci.xammp, "apache/conf/extra/httpd-vhosts.conf");
-// }
-
-void f_append(const char *path, const char *text)
+void file_append(const char *path, const char *text)
 {
     FILE *fd;    
     fd = fopen(path, "a");
@@ -113,25 +45,51 @@ void f_append(const char *path, const char *text)
     fclose(fd);
 }
 
-void append_to_host()
+Cli_info *new_Cli()
 {
-    char *hosts_code = host_code(webiste);
-    f_append(host_path, hosts_code);
-}
+    char *vhost = ":/xampp/apache/conf/extra/httpd-vhosts.conf";
+    char *htdocs = ":/xampp/htdocs/";
 
-void append_to_vhost()
-{
-     char *vhost_content = vhost_code(webiste, webiste_path);
-     f_append(vhost_path, vhost_content);
+    Cli_info *info = malloc(sizeof (Cli_info));
+    info->addr = read_input("Web Name: ", 100);
+    info->drive = read_input("XAMPP Location: ", 100);
+
+    size_t len = strlen(htdocs)+strlen(info->addr)+strlen(info->drive)+1;
+    info->addr_location = malloc(sizeof(char) * len);
+    info->vhost = malloc(sizeof(char) * strlen(vhost)+strlen(info->drive)+1);
+
+    // append 
+    strcpy(info->addr_location, info->drive);
+    strcat(info->addr_location, htdocs);
+    strcat(info->addr_location, info->addr);
+
+    strcpy(info->vhost, info->drive);
+    strcat(info->vhost, vhost);
+        
+    return info;
 }
 
 int main()
 {
-    init_cli();
-    // init_cli_str();
-    create_site_dir(); // create dir
-    append_to_host();
-    append_to_vhost();
+    Cli_info *info = new_Cli();
+    if (_mkdir(info->addr_location) == -1){
+        printf("Error: unable to create Dir \"%s\"\n", info->addr_location);
+        exit(1);
+    }
+
+    char *hosts_code = host_code(info->addr);
+    file_append(host_path, hosts_code);
+
+    char *vhost_content = vhost_code(info->addr, info->addr_location);
+    file_append(info->vhost, vhost_content);
+   
+    // don't really need to free
+    free(vhost_content);
+    free(hosts_code);
+    free(info);
 
     return 0;
 }
+
+
+
